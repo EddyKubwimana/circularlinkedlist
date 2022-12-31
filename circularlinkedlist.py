6dcbf357289e89c4819a66eaa34dbca02a597e77
Eddy Kubwimana <92218436+EddyKubwimana@users.noreply.github.com>
2022-12-31 23:55:46 +0000

    Add files via upload
    added self.size attribute to track the size of the circular linkedlist

--- circularlinkedlist.py
+++ circularlinkedlist.py
@@ -11,14 +11,17 @@ class  circularLinkedList:
         
         self.head = None
         self.tail = None
+        self.size = 0
     def append(self,value):
         ''' add a node at the end of the list'''
         new_node = Node(value)
         if self.head == None:
             self.head = new_node
+            self.size = 1
             return None
         cur_node = self.head
         while cur_node:
+            self.size+=1
             if cur_node.next == None:
                cur_node.next = new_node
                self.tail = cur_node.next
@@ -27,7 +30,30 @@ class  circularLinkedList:
             cur_node = cur_node.next
 
 
+    def insert(self,value, position):
+        if position == 0:
+            self.size +=1
+            new_node = Node(value)
+            new_node.next = self.head
+            self.head = new_node
+            self.tail.next = self.head
+        cur_node = self.head
+        counter = 0
+        
+            
+    def len_list(self):
+           if self.head==0:
+               return 0
+           return self.size
+
+
+
 c = circularLinkedList()
 c.append(10)
 c.append(20)
-print(c.head.next.next.next.next.value)
+c.insert(25,0)
+c.insert(100,0)
+c.insert(200,0)
+print(c.head.next.next.next.value)
+print(c.len_list())
+
